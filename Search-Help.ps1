f34cce1a2f1c0343478175cccbc0d2176e760cd3
Tatsuo Indou <indou.tsystem@gmail.com>
2014-08-19 12:52:25 +0900

    add search-help.ps1

---
+++ Search-Help.ps1
@@ -0,0 +1,41 @@
+###############################################################################
+# Windows Power Shell クックブック P.28
+#
+# Usage:
+#   PS > Search-Help COMMAND
+# ex)
+#   PS > Search-Help hashtable
+#   PS > Search-Help "(datetime|ticks)"
+#
+###############################################################################
+#param($pattern = $(throw "Please specify content to search for"))
+#param($pattern)
+
+# 共通ライブラリの読み込み
+. (Join-Path (Split-Path $MyInvocation.MyCommand.Path) "cmn010.ps1")
+
+###############################################################################
+#
+# 主処理
+#
+###############################################################################
+function main {
+	param($pattern)
+
+	$helpNames = $(Get-Help * | Where-Object { $_.Category -ne "Alias" })
+
+	foreach($helpTopic in $helpNames) {
+		$content = Get-Help -Full $helpTopic.Name | Out-String
+		if ($content -match $pattern) {
+			$helpTopic | Select-Object Name, Synopsis
+		}
+	}
+	exit 0
+}
+
+###############################################################################
+if ($args.length -ne 0) {
+	main $args
+} else {
+	usage $MyInvocation.MyCommand.Path
+}
