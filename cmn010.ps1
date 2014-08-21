430d5e38de77f64a9d83e1d15c0159352d485cf2
Tatsuo Indou <indou.tsystem@gmail.com>
2014-08-21 11:31:09 +0900

    update cmn010.ps1

--- cmn010.ps1
+++ cmn010.ps1
@@ -1,24 +1,24 @@
-
 ###############################################################################
 #
 # usage PATH
 # 	PATH:コマンドレットのパス
-# 	PATHで与えられたファイルを読み込み、最初に見つかった^# Usage:$から、^#$までを返す
+# 	PATHで与えられたファイルを読み込み、最初に見つかった^# *Usage:$から、^# *$までを返す
 #
 ###############################################################################
 function usage {
 	param($path)
-	$encoding = [Text.Encoding]::default 
-	$fh = new-Object IO.StreamReader($path ,$encoding)
+	$encoding = [Text.Encoding]::GetEncoding("Shift_JIS")
+	$fh = new-Object System.IO.StreamReader($path, $encoding)
 	while (($line = $fh.ReadLine()) -ne $null) {
-		if ($line -match "^# Usage:") {
+		if ($line -match "^# *Usage:") {
 			$start = $true
 		}
-		if ($line -match "^#$" -and $start -eq $true) {
+		if ($line -match "^# *$" -and $start -eq $true) {
 			$start = $false
 			break
 		}
 		if ($start) {
+			#$result += "$line`r`n"
 			$line
 		}	
 	}
